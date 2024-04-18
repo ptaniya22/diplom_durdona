e96a3d4fd7a8155c6475ca4df7efb495a302813c
Tiana <tiana.musiyachenko@gmail.com>
2024-04-18 20:45:22 +0500

    changed url brouseriuter

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "@/scss/main.scss";
 import { Provider } from "react-redux";
-import { store } from './redux/index.js';
+import { store } from "./redux/index.js";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter
       // eslint-disable-next-line no-undef
-      basename={process.env.NODE_ENV === "production" ? "/portfolio/" : "/"}
+      basename={
+        process.env.NODE_ENV === "production" ? "/diplom_durdona/" : "/"
+      }
     >
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
