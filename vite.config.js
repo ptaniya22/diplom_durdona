1169342e000dbc6d9b06a3d22b5d7f3c2f27642a
Tiana <tiana.musiyachenko@gmail.com>
2024-04-18 21:03:41 +0500

    install mod

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // eslint-disable-next-line no-undef
+
   base: process.env.NODE_ENV == "production" ? "/diplom_durdona/" : "/",
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src/"),
     },
   },
